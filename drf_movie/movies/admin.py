60e089d42bd40ade14411c79dc7b008569a3cc24
dima <dimdirim95@gmail.com>
2023-07-29 01:19:33 +0200

    add ckeditor and common settings

---
+++ drf_movie/movies/admin.py
@@ -0,0 +1,145 @@
+from django.contrib import admin
+from django import forms
+from django.utils.safestring import mark_safe
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+from .models import Category, Genre, Movie, Review, RatingStar, Rating, Actor, MovieShots
+
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(label='Описание', widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url')
+    list_display_links = ('name',)
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+    readonly_fields = ('name', 'emails')
+
+
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="60" height="auto"')
+
+    get_image.short_description = 'Image'
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category__name')
+    inlines = [MovieShotsInline, ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ('draft',)
+    actions = ['publish', 'unpublish']
+    form = MovieAdminForm
+    readonly_fields = ('get_image',)
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'tagline', 'category'),)
+        }),
+        (None, {
+            'fields': ('description', ('get_image', 'poster'))
+        }),
+        (None, {
+            'fields': (('year', 'world_premiere', 'country'),)
+        }),
+        ('Actors', {
+            'classes': ('collapse',),
+            'fields': (('actors', 'directors', 'genres'),)
+        }),
+        (None, {
+            'fields': (('budget', 'fees_in_usa', 'fees_in_world'),)
+        }),
+        ('Options', {
+            'fields': (('url', 'draft'),)
+        }),
+    )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="50" height="auto"')
+
+    def unpublish(self, request, quetyset):
+        """take off publication"""
+        row_update = quetyset.update(draft=True)
+        if row_update == 1:
+            message_bit = '1 запись была обновлена'
+        else:
+            message_bit = f'{row_update} записей были обновлены'
+        self.message_user(request, f"{message_bit}")
+
+    def publish(self, request, quetyset):
+        """publish"""
+        row_update = quetyset.update(draft=False)
+        if row_update == 1:
+            message_bit = '1 запись была обновлена'
+        else:
+            message_bit = f'{row_update} записей были обновлены'
+        self.message_user(request, f"{message_bit}")
+
+    publish.short_description = 'Publish'
+    publish.allowed_permissions = ('change', )
+
+    unpublish.short_description = 'take off publication'
+    unpublish.allowed_permissions = ('change', )
+
+    get_image.short_description = 'Image'
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'emails', 'parent', 'movie', 'id')
+    readonly_fields = ('name', 'emails')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'get_image')
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="auto"')
+
+    get_image.short_description = 'Image'
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'ip')
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'movie', 'get_image')
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="auto"')
+
+    get_image.short_description = 'Image'
+
+
+admin.site.register(RatingStar)
+
+admin.site.site_title = 'Django Movies'
+admin.site.site_header = 'Django Movies'
