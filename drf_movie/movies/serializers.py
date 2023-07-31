87ca0af72874fd553afd163923c7342b685ea5f6
dima <dimdirim95@gmail.com>
2023-07-31 13:57:20 +0200

    add inserted reviews

--- drf_movie/movies/serializers.py
+++ drf_movie/movies/serializers.py
@@ -3,6 +3,18 @@ from rest_framework import serializers
 from .models import Movie, Review
 
 
+class FilterReviewListSerializer(serializers.ListSerializer):
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveSerializer(serializers.Serializer):
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
 class MovieListSerializer(serializers.ModelSerializer):
     """Movie list"""
 
@@ -20,9 +32,12 @@ class ReviewCreateSerializer(serializers.ModelSerializer):
 
 class ReviewSerializer(serializers.ModelSerializer):
     """Output Review"""
+    children = RecursiveSerializer(many=True)
+
     class Meta:
+        list_serializer_class = FilterReviewListSerializer
         model = Review
-        exclude = ("draft", )
+        fields = ("name", "text", "children")
 
 
 class MovieDetailSerializer(serializers.ModelSerializer):
@@ -31,7 +46,7 @@ class MovieDetailSerializer(serializers.ModelSerializer):
     directors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
     actors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
     genres = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
-    reviews = ReviewCreateSerializer(many=True)
+    reviews = ReviewSerializer(many=True)
 
     class Meta:
         model = Movie
