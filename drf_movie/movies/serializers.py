d8f0207e981313ee4e19d3373bd7e55876628e7a
dima <dimdirim95@gmail.com>
2023-07-31 12:41:06 +0200

    input, ouptut reviews

--- drf_movie/movies/serializers.py
+++ drf_movie/movies/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Movie
+from .models import Movie, Review
 
 
 class MovieListSerializer(serializers.ModelSerializer):
@@ -11,12 +11,27 @@ class MovieListSerializer(serializers.ModelSerializer):
         fields = ("title", "tagline", "category")
 
 
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    """add Review"""
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Output Review"""
+    class Meta:
+        model = Review
+        exclude = ("draft", )
+
+
 class MovieDetailSerializer(serializers.ModelSerializer):
     """Full Movie"""
     category = serializers.SlugRelatedField(slug_field="name", read_only=True)
     directors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
     actors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
     genres = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    reviews = ReviewCreateSerializer(many=True)
 
     class Meta:
         model = Movie
